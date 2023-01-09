60f25044671b2e12ec5879b49851e0264d35f883
Mia-Oh <songmiaoh@gmail.com>
2023-01-09 21:29:02 +0900

    fix:기능에 맞게 Link명 수정

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = (props) => {
           <Nav setShowNav={setShowNav} /> :
           <div        
             onClick={navHandler}
-          >Home</div> 
+          >Nav Bar</div> 
           }
           {showNav ? null :
             <section className="features">
--- src/Nav.js
+++ src/Nav.js
@@ -2,12 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav = ({setShowNav}) => {
-
-
   return (
     <section className="Nav">
-      <div onClick={() => setShowNav(false)}>Home</div>
-      <Link to="/" onClick={() => setShowNav(false)}>List</Link>
+      <div onClick={() => setShowNav(false)}>Nav Bar</div>
+      <Link to="/" onClick={() => setShowNav(false)}>Home</Link>
       <Link to="/about" onClick={() => setShowNav(false)}><p>About</p></Link>
       <Link to="/Lyrics" onClick={() => setShowNav(false)}><p>Lyrics</p></Link>
     </section>
