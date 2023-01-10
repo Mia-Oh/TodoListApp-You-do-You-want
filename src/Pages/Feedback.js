32395f6ecaf1796fa7bc290ea6dabf9cab5c4a3e
Mia-Oh <songmiaoh@gmail.com>
2023-01-10 21:39:55 +0900

    feat:Setting 페이지 추가 및 디자인 적용

--- src/Pages/Feedback.js
+++ src/Pages/Feedback.js
@@ -1,13 +1,29 @@
 import React from "react";
+import Footer from "../Components/Footer";
 
 const Feedback = () => {
   return (
     <section id="feedback">
-      {/* <img src={process.env.PUBLIC_URL + '/clover.png'} alt="clover-logo" /> */}
-      <h3>You do! You Want!</h3>
-      <p>노래 가사와 함께 To do list를 정리해 보세요!</p>
-      <p>행운의 클로버가 그 날의 행운을 가져다 줄거에요</p>
-      <p>그럼 행운을 빌어요!</p>
+      <div id="author">
+        <img src={process.env.PUBLIC_URL + '/author.jpg'} alt="clover-logo" />
+        <div className="cate">
+          <img src={process.env.PUBLIC_URL + '/github.png'} alt="github-logo" />      
+          <a  href="https://github.com/Mia-Oh">github.com/Mia-Oh</a><br />
+        </div>
+        <div className="cate">
+          <img src={process.env.PUBLIC_URL + '/blog.png'} alt="blog-logo" />  
+          <a  href="https://mia-mia.tistory.com/">mia-mia.tistory.com/</a><br />
+        </div>
+        <div className="cate">
+          <img src={process.env.PUBLIC_URL + '/youtube.png'} alt="youtoub-logo" />  
+          <a  href="https://www.youtube.com/@miatube3164">밍튜브 Mia Tube</a><br />
+        </div>
+      </div>
+      <div id="copyright">
+        <p>logo 출처: www.flaticon.com/authors/freepik</p>
+        <p>그외 출처: www.flaticon.com</p>
+      </div>
+      <Footer />
     </section>
   );
 };
